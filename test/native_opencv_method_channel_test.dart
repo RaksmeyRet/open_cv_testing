53971ed18be4f342e92cb4c9ba3f8f6a2b9d88bd
socheatcheng <cheng.socheat@elitevigour.com>
2026-08-14 14:46:03 +0700

    fix: update test imports to package:native_opencv_kit

--- test/native_opencv_method_channel_test.dart
+++ test/native_opencv_method_channel_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:native_opencv/native_opencv_method_channel.dart';
+import 'package:native_opencv_kit/native_opencv_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
--- test/native_opencv_test.dart
+++ test/native_opencv_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:native_opencv/native_opencv.dart';
-import 'package:native_opencv/native_opencv_platform_interface.dart';
-import 'package:native_opencv/native_opencv_method_channel.dart';
+import 'package:native_opencv_kit/native_opencv.dart';
+import 'package:native_opencv_kit/native_opencv_platform_interface.dart';
+import 'package:native_opencv_kit/native_opencv_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockNativeOpencvPlatform
