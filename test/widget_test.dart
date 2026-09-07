52e871cf2afefd1049fdf7fe21df623e3bb19ae7
socheatcheng <cheng.socheat@elitevigour.com>
2026-09-07 09:58:34 +0700

    add

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:native_opencv_kit_example/features/id_scan/domain/entities/scan_result.dart';
+
+import '../lib/features/id_scan/domain/entities/scan_result.dart';
 
 void main() {
   test('reads OCR field aliases and omits empty display values', () {
