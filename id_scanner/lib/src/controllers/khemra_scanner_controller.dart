8749cea17e720e569d81a9a0de82f86a03aae80a
socheatcheng <cheng.socheat@elitevigour.com>
2026-09-25 15:59:52 +0700

    add

--- id_scanner/lib/src/controllers/khemra_scanner_controller.dart
+++ id_scanner/lib/src/controllers/khemra_scanner_controller.dart
@@ -8,7 +8,6 @@ import '../image/image_cropper.dart';
 import '../models/khemra_scan_result.dart';
 import '../ocr/ocr_service.dart';
 import '../utils/scanner_utils.dart';
-import 'package:flutter/services.dart';
 
 class KhemraScannerController extends GetxController
     with GetSingleTickerProviderStateMixin {
@@ -36,12 +35,6 @@ class KhemraScannerController extends GetxController
   @override
   void onInit() {
     super.onInit();
-    
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.landscapeLeft,
-      DeviceOrientation.landscapeRight,
-    ]);
-
     reloadAnimController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 1),
@@ -61,13 +54,6 @@ class KhemraScannerController extends GetxController
 
   @override
   void onClose() {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-      DeviceOrientation.landscapeLeft,
-      DeviceOrientation.landscapeRight,
-    ]);
-
     reloadAnimController.dispose();
     cameraCtrl.value?.dispose();
 
